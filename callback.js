7b6286610585326e8a7df7f662831b22442c62d0
Pushpa R <pushparseervi@gmail.com>
2025-11-18 18:57:18 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,28 @@
+/*function maths(callback) {
+  setTimeout(() => {
+    console.log("maths homework done");
+    callback();
+  }, 10000);
+}
+
+function science(callback) {
+  setTimeout(() => {
+    console.log("science homework done");
+    callback();
+  }, 10000);
+}
+
+function english(callback) {
+  setTimeout(() => {
+    console.log("english homework done");
+    console.log("submit assignment");
+    callback();
+  }, 10000);
+}
+
+maths(function () {
+  science(function () {
+    english(function () {});
+  });
+});
+*/
---
+++ ojects.js
@@ -0,0 +1,32 @@
+/*const student = {
+  name: "pishpa",
+  age:22,
+  action:function(callback){
+   console.log("performing function on:" this.name());
+   callback();
+  }
+}
+student.action(function(){
+ console.log("action completed");
+});
+// object with a callback function
+const areaRectangle = {
+  name: "pushpa",
+  age: 22,
+  branch: "bio",
+  grades: [85, 90, 80, 75],
+  calcavg: function () {
+    let sum = 0;
+    for (let i = 0; i < this.grades.length; i++) {
+      sum += this.greades.length;
+    }
+    return sum / this.grades.length;
+  },
+};
+console.log("average" + student.calcavg());*/
+class Person{
+ constructor(name,age){
+  this.name = name;
+  this._age
+ }
+}
